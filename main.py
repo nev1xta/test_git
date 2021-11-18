62fee4f23e3d47a7a49fa8f54ce5dd766010f791
nevixta <gubiev.david@2007gmail.com>
2021-11-18 18:45:58 +0300

    add commit hash

--- main.py
+++ main.py
@@ -1,10 +1,1 @@
-import this
-
-
-def print_hi(name):
-    print(f'Hi, {name}')
-    #571aa1c228b043e6436e4f60c84f06639cc5985c
-
-
-if __name__ == '__main__':
-    print_hi('PyCharm')
+print('My first git-repo')
